12c3aba905526288d85e5f8e35f0247994085af8
La Diega <83097524+Ladiega@users.noreply.github.com>
2023-11-02 15:16:39 -0500

    working in seciton2

--- app/components/section2/page.js
+++ app/components/section2/page.js
@@ -1,16 +1,31 @@
-import styles from '../../style/section1.module.css'
 
+import styles from '../../style/section2.module.css'
 import Image from 'next/image';
 import section2 from '../../../public/img/section2.png'
 
 export default function Section2() {
   return (
-    <section className={styles.section1_container}>
-      <Image
+    <section className={styles.section2_container}>
+      <div className={styles.section2}>
+        <Image
         alt='Intro Image'
         src={section2}
         layout='responsive'
       />
+      </div>
+      <div className={styles.section2_gallery_container}>
+        <div>
+          Imagen1
+        </div>
+        <div>
+          Imagen2
+        </div>
+        <div>
+          Imagen3
+        </div>
+
+      </div>
+      
     </section>
   )
 }
